83182149af1bf06c741f103dab1b5e9f4032cd38
David B ZHANG <18588744489@163.com>
2022-07-16 16:24:53 +0800

    Update

---
+++ LOAD_PDF.md
@@ -0,0 +1,2 @@
+#### Android 的 WebView 压根就不支持加载 pdf。
+
--- app/src/main/java/com/wwe/MainActivity.kt
+++ app/src/main/java/com/wwe/MainActivity.kt
@@ -2,10 +2,34 @@ package com.wwe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
+    private val mWebView: WebView by lazy {
+        findViewById(R.id.web_container)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mWebView.settings.apply {
+            javaScriptEnabled = true
+            allowFileAccess = true
+            builtInZoomControls = true
+            setSupportZoom(true)
+            displayZoomControls = false //不显示缩放按钮
+        }
+
+        mWebView.loadUrl("file:///android_asset/index.html?https://www.gjtool.cn/pdfh5/git.pdf")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mWebView.apply {
+            clearHistory()
+            clearCache(true)
+            clearFormData()
+        }
     }
 }
